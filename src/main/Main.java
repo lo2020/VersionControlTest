7d3ae7556e73a77267b4c49c948cd3d423f62382
Luke Oaks <oaksluke@gmail.com>
2018-11-30 13:37:13 -0700

    used foo method

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,9 +3,10 @@ package main;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hey, kid");
+        System.out.println(foo());
     }
     
-    public static String foo() {
+    private static String foo() {
         return "this is updated!";
     }
 }
