95668fd42001f1277378bcc8ff91937da4bcfcd6
Luke Oaks <oaksluke@gmail.com>
2018-11-30 13:32:08 -0700

    Added foo method

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hey, kid");
     }
+    
+    public static String foo() {
+        return "this is updated!";
+    }
 }
